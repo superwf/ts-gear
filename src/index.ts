38e84a5d05395dded2ac1d5b4bd7e394695e70c2
superwf <superwf@gmail.com>
2020-05-09 17:32:00 +0800

    fix: add eslint-disable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { requester as fetchRequester } from './requester/fetch'
 export { requester as axiosRequester } from './requester/axios'
-export { processProject } from './run'
 export * from './interface'
